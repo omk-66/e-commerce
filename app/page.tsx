786673d28a782629cc60c5dee75a6ecf1f90c309
om <omkalatheeya100@gmail.com>
2024-09-14 09:35:35 +0530

    add authjs and prisma

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div>
-      hellow world
+    <div className="w-full min-h-screen flex flex-col bg-gradient-to-r from-violet-200 to-pink-200 justify-center items-center">
+      <Button>click me</Button>
     </div>
   );
 }
