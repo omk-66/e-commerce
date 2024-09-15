8903a1ecbe4b5d75b88e86e8134e80c015338620
om <omkalatheeya100@gmail.com>
2024-09-15 17:46:52 +0530

    condintaly render signin and signout button

--- components/navigation/user-button.tsx
+++ components/navigation/user-button.tsx
@@ -1,13 +1,34 @@
 "use client";
+
 import { Session } from "next-auth";
 import { Button } from "../ui/button";
 import { signOut } from "next-auth/react";
-export function UserButton({user}: Session) {
-    return (
-        <div>
-            <div>{user?.email}</div>
-            <div><Button onClick={() => signOut()}>Sign out</Button></div>
-        </div>
-    )
+import Link from "next/link";
+import { IoLogInOutline } from "react-icons/io5";
+
+interface UserButtonProps {
+    user?: Session['user'];
 }
 
+export function UserButton({ user }: UserButtonProps) {
+    return (
+        <div>
+            {user ? (
+                <div>{user.email}</div>
+            ) : null}
+
+            {!user ? (
+                <Link href="api/auth/signin/">
+                    <Button className="flex gap-2">
+                        <IoLogInOutline size={30}/>
+                        <span>Sign in</span>
+                    </Button>
+                </Link>
+            ) : (
+                <div>
+                    <Button onClick={() => signOut()}>Sign out</Button>
+                </div>
+            )}
+        </div>
+    );
+}
