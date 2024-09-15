ab441472d09c00ab3242989e4d7e217c10b5212b
om <omkalatheeya100@gmail.com>
2024-09-15 17:52:04 +0530

    remove expries from user button

--- components/navigation/nav.tsx
+++ components/navigation/nav.tsx
@@ -1,19 +1,19 @@
-import { Logo } from "./logo"
-import { UserButton } from "./user-button"
-import { auth } from "@/auth"
+import { Logo } from "./logo";
+import { UserButton } from "./user-button";
+import { auth } from "@/auth";
+
 export async function NavBar() {
     const session = await auth();
     const user = session?.user;
-    const expires = session?.expires ?? "";
+
     return (
-        <header className="w-full py-4 flex flex-col ">
+        <header className="w-full py-4 flex flex-col">
             <nav>
                 <ul className="flex justify-between px-10">
                     <li><Logo /></li>
-                    <li><UserButton user={user} expires={expires}/></li>
+                    <li><UserButton user={user} /></li>
                 </ul>
             </nav>
         </header>
-    )
+    );
 }
-
--- components/navigation/user-button.tsx
+++ components/navigation/user-button.tsx
@@ -1,26 +1,29 @@
 "use client";
 
-import { Session } from "next-auth";
+import { User } from "next-auth";
 import { Button } from "../ui/button";
 import { signOut } from "next-auth/react";
 import Link from "next/link";
 import { IoLogInOutline } from "react-icons/io5";
 
+// Define props type to expect only user
 interface UserButtonProps {
-    user?: Session['user'];
+    user?: User;
 }
 
 export function UserButton({ user }: UserButtonProps) {
     return (
         <div>
+            {/* Display user email if available */}
             {user ? (
                 <div>{user.email}</div>
             ) : null}
 
+            {/* Conditionally render sign-in or sign-out button */}
             {!user ? (
                 <Link href="api/auth/signin/">
                     <Button className="flex gap-2">
-                        <IoLogInOutline size={30}/>
+                        <IoLogInOutline size={15}/>
                         <span>Sign in</span>
                     </Button>
                 </Link>
